7a42c146f67cb39b68b823a156c2b36df27ef1c0
NormaQuintana <NormaQuintana@gmail.com>
2024-11-05 16:52:50 -0600

    practica11

---
+++ practica11/src/main/java/mx/uv/practica11/Color.java
@@ -0,0 +1,27 @@
+package mx.uv.practica11;
+
+public class Color {
+    String id;
+    String valorColor;
+    String nombreColor;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getValorColor() {
+        return valorColor;
+    }
+    public void setValorColor(String valorColor) {
+        this.valorColor = valorColor;
+    }
+    public String getNombreColor() {
+        return nombreColor;
+    }
+    public void setNombreColor(String nombreColor) {
+        this.nombreColor = nombreColor;
+    }
+   
+}
